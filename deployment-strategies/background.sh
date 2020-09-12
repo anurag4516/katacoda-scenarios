fb35a7998e5692d18a35bb949feecf5595e4932a
Anurag Gupta <anurag4516@gmail.com>
2020-09-12 17:56:12 +0530

    Update background.sh

--- deployment-strategies/background.sh
+++ deployment-strategies/background.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-
 # So we can add and commit files
-git config --global user.email "anurag4516@example.com"
-git config --global user.name "Anurag"
+git config --global user.email "someone@example.com"
+git config --global user.name "Your Name"
 
 # Ensure ansible and ansible-base are not installed so we don't get
 # conflicts when we pip install
+apt-get -y remove ansible 
+pip install ansible-base
+curl -fsSL https://apt.releases.hashicorp.com/gpg |  apt-key add -
+apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+apt-get update && sudo apt-get install terraform
 
-apt-get -y remove ansible ansible-base
-
-
-yum install epel-release -y
-yum install ansible -y
-yum install -y yum-utils
-yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-yum -y install terraform
 git clone https://github.com/anurag4516/thoughtworks_assgnment.git
 cd thoughtworks_assgnment/
 
