56617155414e34b327a328f9bcc749016c17b4b3
Anurag Gupta <anurag4516@gmail.com>
2020-09-12 17:55:15 +0530

    Update background.sh

--- fixing-a-bug/background.sh
+++ fixing-a-bug/background.sh
@@ -6,5 +6,9 @@ git config --global user.name "Your Name"
 
 # Ensure ansible and ansible-base are not installed so we don't get
 # conflicts when we pip install
-apt-get -y remove ansible ansible-base
+apt-get -y remove ansible 
+pip install ansible-base
+curl -fsSL https://apt.releases.hashicorp.com/gpg |  apt-key add -
+apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+apt-get update && sudo apt-get install terraform
 touch /tmp/background-finished
